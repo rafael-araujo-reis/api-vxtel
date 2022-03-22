a09e99ba58081ad63a91366f63055e558db3052a
Rafael Reis <rafa.araujoreis@gmail.com>
2022-03-21 21:02:39 -0300

    router search price minute

--- src/routers/index.js
+++ src/routers/index.js
@@ -16,4 +16,20 @@ router.get('/', (req, res, next) => {
   });
 });
 
+router.get('/callprice/originDDD/:originDDD/destinationDDD/:destinationDDD', async (req, res) => {
+
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS");
+
+  console.log('aqui vou chamar');
+  console.log(req.params);
+
+  res.status(200).send({
+    priceMinute: 10
+  });
+});
+
 module.exports = router;
